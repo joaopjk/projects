5666910c43b962b47425dcca315c22385919d805
joaopjk <joaopjk48@gmail.com>
2024-03-03 11:31:45 -0300

    Configurando conexão

--- GeekShopping/GeekShopping.ProductApi/Program.cs
+++ GeekShopping/GeekShopping.ProductApi/Program.cs
@@ -1,8 +1,16 @@
+using GeekShopping.ProductApi.Model.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+const string connectionString = "Server=127.0.0.1,3306;Database=geek-shopping;Uid=root;Pwd=root@1234";
+builder.Services.AddDbContext<MySqlContext>(c =>
+{
+    c.UseMySql(connectionString, new MySqlServerVersion(new Version(8,2,0)));
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
