14a28d28cfabb292b4dda599221f7091a13d8147
joaopjk <joaopjk48@gmail.com>
2024-03-10 11:57:19 -0300

    Checkpoint

---
+++ GRPCProject/Client/Program.cs
@@ -0,0 +1,18 @@
+﻿using Dummy;
+using Grpc.Core;
+
+const string Target = "127.0.0.1:5005";
+
+var channel = new Channel(Target, ChannelCredentials.Insecure);
+
+channel.ConnectAsync().ContinueWith(task =>
+{
+    if(task.Status == TaskStatus.RanToCompletion)
+        Console.WriteLine("The client connect successfully!");
+});
+
+var client = new DummyService.DummyServiceClient(channel);
+
+channel.ShutdownAsync().Wait();
+
+Console.ReadKey();
---
+++ GRPCProject/Server/Program.cs
@@ -0,0 +1,25 @@
+﻿using Grpc.Core;
+
+const int Port = 5005;
+Server server = null;
+try
+{
+    server = new Server
+    {
+        Ports = { new ServerPort("localhost", Port, ServerCredentials.Insecure) }
+    };
+
+    server.Start();
+    Console.WriteLine($"The server is listening on the port: {Port}");
+
+    Console.ReadKey();
+}
+catch (IOException e)
+{
+    Console.WriteLine(e.Message);
+    throw;
+}
+finally
+{
+    server?.ShutdownAsync().Wait();
+}
